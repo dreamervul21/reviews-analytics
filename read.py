14a577b38a25978298caafe757828c6568216ebe
Joy Wang <dreamervul21@gmail.com>
2021-06-05 19:54:13 +0800

    word count

--- read.py
+++ read.py
@@ -9,15 +9,63 @@ with open('reviews.txt','r') as f:
 		    print(len(data))
 print('檔案讀取完了,總共有',len(data),'筆資料')
 
+
 sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 	print(sum_len)
 print('留言平均長度是',sum_len / len(data))
 
-new = []
+print(data[0])
+
+wc = {} #word_count
 for d in data:
-	if len(d) < 100:
-		new.append(d)
-print('一共有',len(new),'筆留言長度小於100')
-print(new[0])
+	words = d.split(' ')
+	for word in words:
+		if word in wc:
+			wc[word] += 1 
+		else:
+		    wc[word] = 1 #新增新的KEY進WC字典 
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+print(len(wc))
+
+while True:
+	word = input('請問你想查什麼字：')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現過的次數：', wc[word])
+	else:
+		print('這個字沒出現過！')
+
+print('感謝使用本功能')
+
+
+
+
+#new = []
+#for d in data:
+#	if len(d) < 100:
+#		new.append(d)
+#print('一共有',len(new),'筆留言長度小於100')
+#print(new[0])
+
+#good = []
+#for d in data:
+#	if 'good' in d:
+#		good.append(d)
+#print('一共有',len(good),'筆留言')
+#print(good[0])
+
+
+#good =[d for d in data if 'good' in d]
+
+#bad = ['bad' in d for d in data]
+#print(bad)
+
+
+
+
+
